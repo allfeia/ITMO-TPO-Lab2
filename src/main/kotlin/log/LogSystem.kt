fe5b28e1f7357964347db88d59cb71ab60f7d14c
Gerunia <braineaterofficial7200@gmail.com>
2026-03-27 14:26:57 +0300

    new task

--- src/main/kotlin/log/LogSystem.kt
+++ src/main/kotlin/log/LogSystem.kt
@@ -9,11 +9,12 @@ class LogSystem {
                 "LogSystem defined only for x > 0, got $x"
             }
 
+            val ln = Ln.calculate(x, eps)
             val log2 = Log.calculate(x, 2.0, eps)
             val log5 = Log.calculate(x, 5.0, eps)
             val log10 = Log.calculate(x, 10.0, eps)
 
-            return (log2.pow(2.0) * log10 + log5) / log2 + log10
+            return (((log10.pow(3.0) * log5) / ln).pow(3.0)) / log10 + ln + log10
         }
     }
 }
